f2df17068bdd854568985f21bf0280946623a2b0
Vencixa2 <157669079+Vencixa2@users.noreply.github.com>
2024-04-22 06:44:28 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,12 @@
+const popup = document.getElementById("content");
+    function togglePopup() {
+        popup.classList.toggle("active");
+    }
+    function removeClass() {
+        popup.classList.remove("active");
+    }
+
+const background = document.getElementById("body");
+    function changeBackground() {
+        background.classList.toggle("dark");
+    }
